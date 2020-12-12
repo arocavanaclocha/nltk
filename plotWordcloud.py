d7e53f5fb029c975c1bedb50ddd18f3624c27cd3
Alberto <75672168+arocavanaclocha@users.noreply.github.com>
2020-12-12 10:43:03 +0100

    Update plotWordcloud.py

--- plotWordcloud.py
+++ plotWordcloud.py
@@ -1,29 +1,16 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import nltk
 
-def plotWordcloud(remarks):
-  complete_review_text= ""
-  complete_review_text = remarks
-  stop = "NaN"
+def plotWordcloud(tokens):
 
-  wordcloud = WordCloud(stopwords=stop, background_color="white").generate(complete_review_text)
+  wordcloud = WordCloud(background_color="white").generate(str(tokens))
 
   plt.imshow( wordcloud, interpolation='bilinear')
   plt.axis("off")
   plt.figure(figsize = (30,30))    
   plt.show()
 
-def plotFreqDist(remarks, stopwords):   
-  tokens = [t for t in remarks.split()]
-  clean_tokens = tokens[:]        
-
-  for token in tokens:
-      if token in stopwords.words('english'):
-          clean_tokens.remove(token)
-
-  freq = nltk.FreqDist(clean_tokens)
-  freq.plot(20, cumulative=False)
-  
-
-def addStopwords( lang, stopwords):
-  sess.Stopwords[0][lang]  += stopwords
+def plotFreqDist(tokens):    
+  freq = nltk.FreqDist(tokens)
+  freq.plot(20, cumulative=False)  
